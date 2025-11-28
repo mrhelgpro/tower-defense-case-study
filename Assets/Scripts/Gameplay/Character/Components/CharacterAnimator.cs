2c680b16aeeda5f4ee9672f329f382bf6d485bb5
mrhelgpro <mrhelgpro@gmail.com>
2025-11-28 18:06:51 +0200

    Animations added

--- Assets/Scripts/Gameplay/Character/Components/CharacterAnimator.cs
+++ Assets/Scripts/Gameplay/Character/Components/CharacterAnimator.cs
@@ -4,19 +4,24 @@ namespace TowerDefence.Gameplay
 {
     public class CharacterAnimator : MonoBehaviour
     {
+        private static readonly int IS_MOVE = Animator.StringToHash("isMove");
+        private static readonly int ATTACK = Animator.StringToHash("Attack");
+        
+        [SerializeField] private Animator _animator;
+        
         public void PlayMove()
         {
-            
+            _animator.SetBool(IS_MOVE, true);
         }
 
         public void PlayIdle()
         {
-            
+            _animator.SetBool(IS_MOVE, false);
         }
 
         public void PlayAttack()
         {
-            
+            _animator.Play(ATTACK);
         }
     }
 }
